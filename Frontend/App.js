2180970e7056ad1150d174b155b09d62521f1866
Napus Samuanpho <napus.dev@gmail.com>
2025-03-03 17:16:24 +0700

    ReApp

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,32 +1,20 @@
-import React from 'react'; 
-import { NavigationContainer } from '@react-navigation/native'; 
-import { createStackNavigator } from '@react-navigation/stack';
-import TabNavigator from './navigators/TabNavigator';
-import ApplyMechanicScreen from './screens/Account/ApplyMechanicScreen';
-import LogInScreen from './screens/Register/LogInScreen';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const Stack = createStackNavigator();
-
-export default function App() { 
-  return ( 
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={'LogIn'}>
-        <Stack.Screen
-          name='LogIn'
-          component={LogInScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='Tab'
-          component={TabNavigator}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='ApplyMechanic'
-          component={ApplyMechanicScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer> 
-  ); 
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
